656cda70aaca7ee192aa862d8976a8e27ff9d668
natwltz <nathan.waltzer@stagiairesmns.fr>
2026-03-19 09:46:58 +0100

    Ajout de la vérification de mot de passe (pas encore fonctionnel)

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "../include/config.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "../include/connect.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "../include/function.php";
+
+$errorMessage = "";
+if (!empty($_POST["password"]) && !empty($_POST["mail"])) {
+    $errorMessage = "<p>Le mail ou le mdp sont incorrect</p>";
+    $sql = "SELECT * 
+            FROM users
+            WHERE users_mail = :mail";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([":mail" => $_POST["mail"]]);
+    if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        if (password_verify($_POST["password"], $row["users_pwd"])) {
+            session_start();
+            if ($row["users_is_admin"] == 1) {
+                $_SESSION["is_admin"] = true;
+                //Vérifie si l'utilisateur est admin,
+                //si oui on l'inscrit dans la session
+            } else {
+                $_SESSION["is_admin"] = false;
+            }
+            $_SESSION["users_connected"] = "ok";
+            $_SESSION["users_id"] = $row["users_id"];
+            header("Location:users/home.php");
+            exit;
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +42,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet">
 </head>
+
 <body class="page-connexion">
 
     <header class="en-tete">
@@ -19,22 +52,24 @@
     <main class="conteneur-central">
         <h1 class="titre-connexion">Connexion</h1>
 
-        <form class="formulaire">
+        <form class="formulaire" action="index.php" method="post">
             <div class="groupe-saisie">
-                <label for="username">Username :</label>
-                <input type="text" id="username" class="champ-texte">
+                <label for="mail">Mail :</label>
+                <input type="email" name="mail" id="mail" class="champ-texte" required>
             </div>
 
             <div class="groupe-saisie">
                 <label for="password">Mot de passe :</label>
-                <input type="password" id="password" class="champ-texte">
+                <input type="password" name="password" id="password" class="champ-texte" required>
             </div>
 
-            <button type="submit" class="bouton-primaire bouton-large">Connexion</button>
+            <button type="submit" value="OK" class="bouton-primaire bouton-large">Connexion</button>
+            <?= $errorMessage ?>
         </form>
 
         <a href="#" class="lien-oubli">Mot de passe oublié</a>
     </main>
 
 </body>
+
 </html>
